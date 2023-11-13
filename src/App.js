dfb7636b66342469c29b7c494d792c077cda0e8d
Vishal Baliyan <vishalbaliyan@clearwateranalytics.com>
2023-11-13 14:13:20 +0530

    creating project and setting up tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="bg-red-100 font-serif text-green-500">
+      Welcome to React App thats build using Webpack and Babel separately
+    </h1>
+  );
+};
+
+export default App;
